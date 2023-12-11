48869e54f888f380e430667b182f7b3c14217a22
sumanth010895 <146088625+sumanth010895@users.noreply.github.com>
2023-12-11 16:04:14 +0500

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/SMS_Devops
-virtualenv /home/ubuntu/SMS_Devops/venv
-source /home/ubuntu/SMS_Devops/venv/bin/activate
-pip install -r /home/ubuntu/SMS_Devops/requirements.txt
+sudo chown -R ubuntu:ubuntu ~/student_crm
+virtualenv /home/ubuntu/student_crm/venv
+source /home/ubuntu/student_crm/venv/bin/activate
+pip install -r /home/ubuntu/student_crm/requirements.txt
