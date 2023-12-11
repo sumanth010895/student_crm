fe34636023f086be2b90e3ffc24de9392c5a5465
sumanth010895 <146088625+sumanth010895@users.noreply.github.com>
2023-12-11 16:02:25 +0500

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/SMS_Devops/nginx/nginx.conf /etc/nginx/sites-available/django_project
+sudo cp /home/ubuntu/student_crm/nginx/nginx.conf /etc/nginx/sites-available/django_project
 sudo ln -s /etc/nginx/sites-available/django_project /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
