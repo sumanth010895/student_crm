336a265a6450ae39beb173c8c276e651750f7d34
sumanth010895 <146088625+sumanth010895@users.noreply.github.com>
2023-12-11 15:59:42 +0500

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-sudo cp /home/ubuntu/SMS_Devops/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/SMS_Devops/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
+sudo cp /home/ubuntu/student_crm/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/student_crm/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
 sudo systemctl start gunicorn.service
 sudo systemctl enable gunicorn.service
